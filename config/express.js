d6be3b6357fb7d3625c2d51a08550bc1434cc52a
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2016-06-09 19:49:50 -0300

    Capítulo 2

--- config/express.js
+++ config/express.js
@@ -1,14 +1,30 @@
 //config/express.js
-var express = require('express');
+var express = require('express')
+var home = require('../app/routes/home');
+var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function() {
     var app = express();
 
     //configuração de ambiente
     app.set('port', 3000);
-
-    //middleware
-    app.use(express.static('./public'));
-
+    
+    // abaixo do middleware express.static
+    app.set('view engine', 'ejs');
+    app.set('views','./app/views');
+    
+    load('models', {cwd: 'app'})
+     .then('controllers')
+     .then('routes')
+     .into(app);
+    
     return app;
+  
+  // middleware
+  app.use(express.static('./public'));
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(require('method-override')())
+
 };
